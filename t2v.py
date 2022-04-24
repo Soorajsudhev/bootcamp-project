23bbad5d7ab13489c385de32acf9b5433640eeae
Jeevagan <64369514+jeevagan1@users.noreply.github.com>
2022-04-24 16:25:51 +0530

    Update t2v.py

--- t2v.py
+++ t2v.py
@@ -1,7 +1,10 @@
-from gtts import gtts
+from gtts import gTTS
 import os
-text = input("enter txt : ")
-lang = "en"
-speech = gTTS(text=text, lang=lang, slow=False)
-speech.save("voice.mp3")
-os.system("mpg321 voice.mp3")
+
+while True:
+        text = input("enter txt : ")
+        lang = "en"
+        speech = gTTS(text=text, lang=lang, slow=False)
+        speech.save("voice.mp3")
+        os.system("mpg321 voice.mp3")
+        pass
